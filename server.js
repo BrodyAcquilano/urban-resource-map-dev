f36f27634fa18265008fabd582a9f394549dfebc
Brody Acquilano <brodya2621@gmail.com>
2025-06-15 15:35:37 -0400

    Add CORS support for frontend requests

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import connectToMongo from "./db.js";
 import locationRoutes from "./routes/locations.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
